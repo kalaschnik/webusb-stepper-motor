673d00e55c51db06546941fa2ae2eb5a19f25063
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-10-20 09:18:52 +0200

    add example to send and receive data browser ↔ arduino

--- webusb/webusb.ino
+++ webusb/webusb.ino
@@ -1,19 +1,49 @@
 #include <WebUSB.h>
+WebUSB WebUSBSerial(1, "127.0.0.1:5500/src/");
 
-WebUSB WebUSBSerial(1 /* https:// */, "webusb.github.io/arduino/demos/");
+const int ledPin = 13;
 
 void setup()
 {
 	WebUSBSerial.begin(9600);
+	// needs empty while blocks in setup(), otherwise print() does not work
 	while (!WebUSBSerial)
-	{
 		;
-	}
+	while (!SerialUSB)
+		;
+
+	// WebUSB output (console)
+	// flush is needed, otherwise there is no ouput in the browser’s console
+	WebUSBSerial.println("WebUSB Serial ready!");
+	WebUSBSerial.flush();
+
+	// Serial Monitor output
+	SerialUSB.println("Starting from SerialUSB");
+
+	pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
-	WebUSBSerial.println("Hello from Arduino to Browser");
-	WebUSBSerial.flush();
-	delay(1000);
+	if (WebUSBSerial && WebUSBSerial.available())
+	{
+		// read input from the browser
+		int byte = WebUSBSerial.read();
+		WebUSBSerial.write(byte);
+
+		if (byte == 'H')
+		{
+			digitalWrite(ledPin, HIGH);
+			SerialUSB.println("received H");
+			WebUSBSerial.println("Tuning on the LED");
+		}
+		else if (byte == 'L')
+		{
+			digitalWrite(ledPin, LOW);
+			SerialUSB.println("received L");
+			WebUSBSerial.println("Tuning off the LED");
+		}
+
+		WebUSBSerial.flush();
+	}
 }
